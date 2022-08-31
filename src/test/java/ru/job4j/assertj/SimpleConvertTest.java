c11d4bb04898637a6a783f399a7a5eea46aad40c
Olegsander48 <olegsander2001@gmail.com>
2022-08-31 10:21:49 +0300

    Утверждения с коллекциями [#504885]

---
+++ src/test/java/ru/job4j/assertj/SimpleConvertTest.java
@@ -0,0 +1,55 @@
+package ru.job4j.assertj;
+
+import org.assertj.core.data.Index;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.*;
+
+class SimpleConvertTest {
+    @Test
+    void checkArray() {
+        SimpleConvert simpleConvert = new SimpleConvert();
+        String[] array = simpleConvert.toArray("first", "second", "three", "four", "five");
+        assertThat(array).hasSize(5)
+                .contains("second")
+                .contains("first", Index.atIndex(0))
+                .containsAnyOf("zero", "second", "six")
+                .doesNotContain("first", Index.atIndex(1));
+    }
+
+    @Test
+    void checkList() {
+        SimpleConvert simpleConvert = new SimpleConvert();
+        List<String> list = simpleConvert.toList("first", "second", "three", "four", "five");
+        assertThat(list).isNotNull()
+                .anySatisfy(e -> assertThat(e).isEqualTo("first"))
+                .containsAnyOf("five", "six", "seven")
+                .contains("second", "four")
+                .first().isEqualTo("first");
+    }
+
+    @Test
+    void checkSet() {
+        SimpleConvert simpleConvert = new SimpleConvert();
+        Set<String> set = simpleConvert.toSet("first", "second", "three", "four", "five");
+        assertThat(set).isNotEmpty()
+                .hasSize(5)
+                .allMatch(e -> e.length() >= 4)
+                .noneMatch(e -> e.length() < 3)
+                .element(0)
+                .isNotEqualTo("five");
+    }
+
+    @Test
+    void checkMap() {
+        SimpleConvert simpleConvert = new SimpleConvert();
+        Map<String, Integer> map = simpleConvert.toMap("first", "second", "three", "four", "five");
+        assertThat(map).isNotEmpty()
+                .hasSize(5)
+                .containsKey("second")
+                .containsValue(3)
+                .containsEntry("first", 0);
+    }
+}
