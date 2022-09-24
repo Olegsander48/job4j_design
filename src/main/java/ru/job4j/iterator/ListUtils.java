9c94032e217b88aa6a384f2f9ab035c4e1aefb76
Olegsander48 <prigodichaleks@gmail.com>
2022-09-24 13:28:37 +0300

    7. ListIterator [#350217]

---
+++ src/main/java/ru/job4j/iterator/ListUtils.java
@@ -0,0 +1,66 @@
+package ru.job4j.iterator;
+
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class ListUtils {
+
+    public static <T> void addBefore(List<T> list, int index, T value) {
+        Objects.checkIndex(index, list.size());
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (iterator.nextIndex() == index) {
+                iterator.add(value);
+                break;
+            }
+            iterator.next();
+        }
+    }
+
+    public static <T> void addAfter(List<T> list, int index, T value) {
+        Objects.checkIndex(index, list.size());
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (iterator.previousIndex() == index) {
+                iterator.add(value);
+                break;
+            }
+            iterator.next();
+        }
+    }
+
+    public static <T> void removeIf(List<T> list, Predicate<T> filter) {
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            T el = iterator.next();
+            if (filter.test(el)) {
+                iterator.remove();
+            }
+        }
+    }
+
+    public static <T> void replaceIf(List<T> list, Predicate<T> filter, T value) {
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            T el = iterator.next();
+            if (filter.test(el)) {
+                iterator.set(value);
+            }
+        }
+    }
+
+    public static <T> void removeAll(List<T> list, List<T> elements) {
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            T el = iterator.next();
+            ListIterator<T> elIterator = elements.listIterator();
+            while (elIterator.hasNext()) {
+                T el1 = elIterator.next();
+                if (el == el1) {
+                    iterator.remove();
+                }
+            }
+        }
+    }
+}
---
+++ src/test/java/ru/job4j/iterator/ListUtilsTest.java
@@ -0,0 +1,56 @@
+package ru.job4j.iterator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.*;
+
+class ListUtilsTest {
+
+    private List<Integer> input;
+
+    @BeforeEach
+    void setUp() {
+        input = new ArrayList<>(Arrays.asList(1, 3));
+    }
+
+    @Test
+    void whenAddBefore() {
+        ListUtils.addBefore(input, 1, 2);
+        assertThat(input).hasSize(3).containsSequence(1, 2, 3);
+    }
+
+    @Test
+    void whenAddBeforeWithInvalidIndex() {
+        assertThatThrownBy(() -> ListUtils.addBefore(input, 3, 2))
+                .isInstanceOf(IndexOutOfBoundsException.class);
+    }
+
+    @Test
+    void whenAddAfter() {
+        ListUtils.addAfter(input, 0, 2);
+        assertThat(input).hasSize(3).containsSequence(1, 2, 3);
+    }
+
+    @Test
+    void whenRemoveEvenNumbers() {
+        input = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+        ListUtils.removeIf(input, el -> el % 2 == 0);
+        assertThat(input).hasSize(3).containsSequence(1, 3, 5);
+    }
+
+    @Test
+    void whenReplaceEvenNumbers() {
+        input = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+        ListUtils.replaceIf(input, el -> el % 2 == 0, 1000);
+        assertThat(input).hasSize(6).containsSequence(1, 1000, 3, 1000, 5, 1000);
+    }
+
+    @Test
+    void whenRemoveDuplicateNumbersFromBothLists() {
+        input = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 4, 5, 6, 7, 8));
+        ListUtils.removeAll(input, Arrays.asList(3, 4, 5, 8));
+        assertThat(input).hasSize(4).containsSequence(1, 2, 6, 7);
+    }
+}
