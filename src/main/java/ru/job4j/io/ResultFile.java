7f134b1b3e68e7cdf5e88c6c54373b89ec986877
Olegsander48 <prigodichaleks@gmail.com>
2023-01-03 12:13:06 +0300

    0.1. FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,24 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+import java.util.Scanner;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Input the size of multiplication table: ");
+            int size = scanner.nextInt();
+            scanner.close();
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    out.write(((i + 1) * (j + 1) + " ").getBytes());
+                }
+                out.write(System.lineSeparator().getBytes());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
