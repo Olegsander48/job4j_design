c6a2e1d4fff737ff090b2eea71f61588bbbfb98f
Olegsander48 <prigodichaleks@gmail.com>
2023-01-09 11:41:52 +0300

    5. Валидация параметров запуска. [#246865 #378849]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,10 +10,10 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        if (checkArguments(args)) {
-            Path start = Paths.get(args[0]);
-            search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
-        }
+        checkArguments(args);
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
+
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
@@ -21,15 +22,23 @@ public class Search {
         return searcher.getPaths();
     }
 
-    public static boolean checkArguments(String[] args) {
+    private static void checkArguments(String[] args) {
 
-        if (args.length < 1) {
+        if (args.length != 2) {
             throw new IllegalArgumentException("Root folder and file extension is null. Usage  ROOT_FOLDER and FILE_EXTENSION.");
         }
-        if (args.length < 2) {
-            throw new IllegalArgumentException("File extension is null. Usage  FILE_EXTENSION.");
+
+        File file = new File(args[0]);
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
         }
-        return true;
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+
+        if (args[1].length() < 1) {
+            throw new IllegalArgumentException("File extension is null. Please, usage FILE_EXTENSION ");
+        }
     }
 }
 
