b4284292de65a2718c94b1fada196df5f630ae44
Olegsander48 <prigodichaleks@gmail.com>
2025-02-10 16:50:46 +0300

    forget delete debug information

--- src/main/java/ru/job4j/io/Config.java
+++ src/main/java/ru/job4j/io/Config.java
@@ -24,7 +24,6 @@ public class Config {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println("Мапа" + values);
     }
 
     public String value(String key) {
