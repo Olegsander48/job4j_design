d904a533c0f5e82be9949dbb002a1c83a0dac13d
Olegsander48 <prigodichaleks@gmail.com>
2025-02-11 13:14:59 +0300

    12. Валидация параметров запуска [#246865 #378849]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,13 +9,26 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, path -> path.toFile().getName().endsWith(".txt")).forEach(System.out::println);
+        validateArguments(args);
+        Path start = Paths.get(args[0]);
+        search(start, path -> path.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    private static void validateArguments(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage  ROOT_FOLDER.");
+        }
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Invalid number of arguments. Expected 2, but was " + args.length);
+        }
+        if (!args[1].startsWith(".") || args[1].length() < 2) {
+            throw new IllegalArgumentException("Invalid format of file extension. Use .FILE_EXTENSION");
+        }
     }
 }
