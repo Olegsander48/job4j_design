2c3829c1bdcb15b59a82558accbfe564e9b02137
Olegsander48 <prigodichaleks@gmail.com>
2025-03-12 10:01:54 +0300

    change code for using statement

--- src/main/java/ru/job4j/jdbc/ConnectionDemo.java
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -2,23 +2,49 @@ package ru.job4j.jdbc;
 
 import ru.job4j.io.Config;
 
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.StringJoiner;
 
 public class ConnectionDemo {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        Config config = new Config("data/connection.properties");
+    public static void main(String[] args) throws Exception {
+        try (Connection connection = getConnection()) {
+            try (Statement statement = connection.createStatement()) {
+                String sql = String.format(
+                        "CREATE TABLE IF NOT EXISTS demo_table(%s, %s);",
+                        "id SERIAL PRIMARY KEY",
+                        "name TEXT"
+                );
+                statement.execute(sql);
+                System.out.println(getTableScheme(connection, "demo_table"));
+            }
+        }
+    }
+
+    private static Connection getConnection() throws Exception {
+        Config config = new Config("src/main/resources/connection.properties");
         config.load();
-        Class.forName(config.value("jdbc.connection.driver_class"));
-        try (Connection connection = DriverManager.getConnection(
+        return DriverManager.getConnection(
                 config.value("jdbc.connection.url"),
                 config.value("jdbc.connection.username"),
-                config.value("jdbc.connection.password"))) {
-            DatabaseMetaData metaData = connection.getMetaData();
-            System.out.println(metaData.getUserName());
-            System.out.println(metaData.getURL());
+                config.value("jdbc.connection.password"));
+    }
+
+    public static String getTableScheme(Connection connection, String tableName) throws Exception {
+        var rowSeparator = "-".repeat(30).concat(System.lineSeparator());
+        var header = String.format("%-15s|%-15s%n", "NAME", "TYPE");
+        var buffer = new StringJoiner(rowSeparator, rowSeparator, rowSeparator);
+        buffer.add(header);
+        try (var statement = connection.createStatement()) {
+            var selection = statement.executeQuery(String.format(
+                    "SELECT * FROM %s LIMIT 1", tableName
+            ));
+            var metaData = selection.getMetaData();
+            for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                buffer.add(String.format("%-15s|%-15s%n",
+                        metaData.getColumnName(i), metaData.getColumnTypeName(i))
+                );
+            }
         }
+        return buffer.toString();
     }
 }
